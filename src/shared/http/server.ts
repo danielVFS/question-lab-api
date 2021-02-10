1555ec6c1fd283d621610cd23bfb8b37ab4b20a8
danielVFS <danielv759153@gmail.com>
2021-02-10 17:17:31 -0300

    ErrorHandler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import routes from '../routes/index';
+
+import ErrorHandler from '@shared/errors/ErrorHandler';
 
 const app = express();
 
@@ -9,6 +11,22 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ErrorHandler) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  },
+);
+
 app.listen(3333, () => {
-  console.log('Server running on port 3333');
+  console.log('Server running on port 3333 😈');
 });
