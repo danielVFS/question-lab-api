989b6269839f2ba0916cf6bd6e8d16f2790aa5a1
danielVFS <danielv759153@gmail.com>
2021-02-10 18:41:08 -0300

    feat: Question entitie

---
+++ src/modules/questions/typeorm/entities/Question.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('questions')
+class Question {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Question;
