8503d5d13a6df65d75e67d9b8404f7285588b471
danielVFS <danielv759153@gmail.com>
2021-02-10 18:52:48 -0300

    feat: CreateQuestionService

---
+++ src/modules/questions/services/CreateQuestionService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+
+import Question from '../typeorm/entities/Question';
+import QuestionsRepository from '../typeorm/repositories/QuestionsRepository';
+
+interface IRequest {
+  title: string;
+  description: string;
+}
+
+class CreateQuestionService {
+  public async execute({ title, description }: IRequest): Promise<Question> {
+    const questionsRepository = getCustomRepository(QuestionsRepository);
+
+    const question = await questionsRepository.create({
+      title,
+      description,
+    });
+
+    return question;
+  }
+}
+
+export default CreateQuestionService;
