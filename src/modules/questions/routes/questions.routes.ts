9e264775a85c4f2db744b4dd9b13689b0c8384bc
danielVFS <danielv759153@gmail.com>
2021-02-10 21:05:03 -0300

    validantes params and body with celebrate

--- src/modules/questions/routes/questions.routes.ts
+++ src/modules/questions/routes/questions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import QuestionsController from '../controllers/QuestionsController';
 
@@ -6,10 +7,47 @@ const questionRoutes = Router();
 
 const questionsController = new QuestionsController();
 
-questionRoutes.post('/', questionsController.create);
+questionRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+    },
+  }),
+  questionsController.create,
+);
 questionRoutes.get('/', questionsController.index);
-questionRoutes.get('/:id', questionsController.show);
-questionRoutes.put('/:id', questionsController.update);
-questionRoutes.delete('/:id', questionsController.delete);
+questionRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  questionsController.show,
+);
+questionRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+    },
+  }),
+  questionsController.update,
+);
+questionRoutes.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  questionsController.delete,
+);
 
 export default questionRoutes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import ErrorHandler from '@shared/errors/ErrorHandler';
 
@@ -14,6 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors);
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
